863d245704f0ddcc2929ff499005aa8067a5122c
Jamil <gafforovabdujamil@gmail.com>
2024-08-27 15:34:01 +0500

    add smooth scroll on click btn up

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 document.querySelectorAll('.menu__link').forEach(link => {
-    link.addEventListener('click', function() {
+    link.addEventListener('click', function () {
         document.querySelectorAll('.menu__link').forEach(item => item.classList.remove('active'));
         this.classList.add('active');
     });
 });
 
-document.getElementById('menuIcon').addEventListener('click', function() {
+document.getElementById('menuIcon').addEventListener('click', function () {
     const popupMenu = document.getElementById('popupMenu');
     this.classList.toggle('open');
     popupMenu.classList.toggle('open');
+});
+
+
+document.querySelector('.btn__up').addEventListener('click', function (e) {
+    e.preventDefault(); 
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
